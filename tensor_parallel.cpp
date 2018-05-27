8a424f7b3950b50c1b0268e233c1446b626fc855
evanzhao <evanzhao@umich.edu>
2018-05-27 16:17:37 -0400

    Modify tensor_parallel to take env variable as size

--- tensor_parallel.cpp
+++ tensor_parallel.cpp
@@ -1,12 +1,6 @@
 #include <unsupported/Eigen/CXX11/Tensor>
 #include <iostream>
 
-#ifdef MSIZE
-#define SIZE MSIZE
-#else
-#define SIZE 1000
-#endif
-
 using namespace std;
 
 /*void printResult()
@@ -24,35 +18,38 @@ using namespace std;
   fclose(fp);
 }*/
 
-int main(void)
-{
-  //cout<<&c(0,0).value<<endl;
-  //cout<<&c(0,1).value<<endl;
-  //cout<<&c(1,0).value<<endl;
-  // int SIZE;
-  // cin>>SIZE;
-Eigen::Tensor<float, 2> a(SIZE,SIZE);
+int main(void) {
+    //cout<<&c(0,0).value<<endl;
+    //cout<<&c(0,1).value<<endl;
+    //cout<<&c(1,0).value<<endl;
+    // int SIZE;
+    // cin>>SIZE;
+    char const *size_str = getenv("MSIZE");
+    int SIZE = size_str ? std::stoi(size_str) : 1000;
+    cout << SIZE << '\n';
 
-Eigen::Tensor<float, 2> b(SIZE,SIZE);
+    Eigen::Tensor<float, 2> a(SIZE, SIZE);
 
-Eigen::Tensor<float, 2> c(SIZE,SIZE);
+    Eigen::Tensor<float, 2> b(SIZE, SIZE);
 
-Eigen::array<Eigen::IndexPair<int>, 1> dimension = {Eigen::IndexPair<int>(1,0)};
-  Eigen::ThreadPool tpi(8);
-  Eigen::ThreadPoolDevice my_device(&tpi, 8);
-  // for(int i = 0; i < SIZE; i++)
-  // {
-  //   for(int j = 0; j < SIZE; j++)
-  //   {
-  //     a(i,j) = i+j;
-  //     b(i,j) = i-j;
-  //   }
-  // }
-  c.device(my_device) = a.contract(b, dimension);
-  //c1 = a1.contract(b1, dimension);
-  /*cout<<a<<endl;
-  cout<<b<<endl;
-  cout<<c<<endl;*/
-  //printResult();
-  return 0;
+    Eigen::Tensor<float, 2> c(SIZE, SIZE);
+
+    Eigen::array<Eigen::IndexPair<int>, 1> dimension = {Eigen::IndexPair<int>(1, 0)};
+    Eigen::ThreadPool tpi(8);
+    Eigen::ThreadPoolDevice my_device(&tpi, 8);
+    // for(int i = 0; i < SIZE; i++)
+    // {
+    //   for(int j = 0; j < SIZE; j++)
+    //   {
+    //     a(i,j) = i+j;
+    //     b(i,j) = i-j;
+    //   }
+    // }
+    c.device(my_device) = a.contract(b, dimension);
+    //c1 = a1.contract(b1, dimension);
+    /*cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;*/
+    //printResult();
+    return 0;
 }
