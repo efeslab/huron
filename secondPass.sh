5e25e924fe78d84b028161dfc03edc114c67be5a
Tanvir Ahmed Khan <takhandipu@gmail.com>
2018-05-30 12:00:00 -0400

    Bugs on second pass is fixed

--- secondPass.sh
+++ secondPass.sh
@@ -1,5 +1,5 @@
 clang++ -c -emit-llvm -std=c++11 -I ../eigen/ -DEIGEN_USE_THREADS tensor_parallel.cpp -o tensor.bc
-opt -load ./LLVMInstLoadStore.so -instloadstore < tensor.bc > tensor_inst.bc 2> build_tensor.log
+opt -load ./LLVMInstLoadStore.so -instloadstore -locfile record_output.log < tensor.bc > tensor_inst.bc 2> build_tensor.log
 mv tensor_inst.bc tensor.bc
 llc -filetype=obj tensor.bc -o tensor.o
-g++ tensor.o -Wl,./secondpass.so -lpthread -o tensor
+g++ tensor.o -Wl,./secondpass.so -lpthread -o tensor_2
