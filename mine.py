df195862baf41786e345a3021dd0319454219977
Tanvir Ahmed Khan <takhandipu@gmail.com>
2018-05-31 11:37:17 -0400

    cacheMiss percentage of false sharing is calculated

---
+++ mine.py
@@ -0,0 +1,18 @@
+import re
+
+regex = r"\s+([\d,]+)\s+cache-misses.+\n\s+([\d,]+)\s+mem_load_uops_l3_hit_retired_xsnp_hitm.+"
+
+test_str = ""
+
+with open("cachemisses.txt", "r") as file:
+  test_str = file.read()
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+
+for matchNum, match in enumerate(matches):
+  matchNum = matchNum + 1
+  # print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+  for groupNum in range(0, len(match.groups())):
+    groupNum = groupNum + 1
+    print "{group};".format(group = match.group(groupNum)),
+  print ''
