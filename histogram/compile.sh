1483ac991fb8c4caace5c24a44609676090b9758
Tanvir Ahmed Khan <takhandipu@gmail.com>
2018-06-05 18:29:50 -0400

    Three new false sharing benchmarks are added to the repository

---
+++ histogram/compile.sh
@@ -0,0 +1,5 @@
+set -x
+clang -c -emit-llvm hist-pthread.c -D_LINUX_ -o a.bc
+opt -load ~/eigen-optim/LLVMInstrumenter.so -instrumenter < a.bc > a.inst.bc 2> a.log
+llc -filetype=obj a.inst.bc -o a.o
+gcc a.o -Wl,/home/takh/eigen-optim/libruntime.so -lpthread -o a.out
