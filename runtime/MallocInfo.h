45388c615f3de7bec5c8add7749a38b0b617fe7a
evanzhao <evanzhao@umich.edu>
2018-06-22 15:07:50 -0400

    Return to lockless techniques

--- runtime/MallocInfo.h
+++ runtime/MallocInfo.h
@@ -47,13 +47,13 @@ class MallocInfo {
         explicit PerBt(uintptr_t _addr, size_t _id, size_t _size)
                 : addr(_addr), id(_id), size(_size) {}
 
-        bool operator < (const PerBt &rhs) const {
+        bool operator<(const PerBt &rhs) const {
             return addr < rhs.addr;
         }
     };
 
     // This is an (ordered) map because we need to query lower bound for incoming access addresses.
-    std::map<uintptr_t, PerAddr> data_alive;
+    std::map<uintptr_t, PerAddr> *data_alive;
     std::unordered_map<std::vector<void *>, std::vector<PerBt>> data_total;
     std::shared_timed_mutex mutex;
     AddrSeg heap;
@@ -61,6 +61,7 @@ class MallocInfo {
 public:
     MallocInfo() : data_total(), heap(~0LU, 0), id(0) {
         HookDeactivator deactiv;
+        data_alive = new std::map<uintptr_t, PerAddr>;
     }
 
     void dump(const char *path) const {
@@ -80,27 +81,37 @@ public:
     }
 
     void insert(uintptr_t start, size_t size) {
-        // This function itself is single threaded,
-        // so not all operations need protection with lock.
+        // There's only one writer (thread 0), so we're doing this to avoid locks.
+        if (data_alive->find(start) == data_alive->end()) {
+            auto *copy = new std::map<uintptr_t, PerAddr>(*data_alive);
+            copy->emplace(start, PerAddr(id, size));
+            // No one can modify data_alive between the following 2 lines, so we're safe.
+            auto *old = data_alive;
+            data_alive = copy;  // atomic!
+            delete old;
+        }
         static void *bt_buf[1000];
         int bt_size = backtrace(bt_buf, 1000);
         std::vector<void *> bt(bt_buf, bt_buf + bt_size);
-        mutex.lock();
-        data_alive[start] = PerAddr(id, size);
-        mutex.unlock();
         heap.insert(AddrSeg(start, start + size));
         data_total[bt].emplace_back(start, id, size);
         id++;
     }
 
     bool erase(uintptr_t addr) {
-        std::unique_lock<std::shared_timed_mutex> ul(mutex);
-        auto it = data_alive.find(addr);
-        if (it == data_alive.end())
+        // There's only one writer (thread 0), so we're doing this to avoid locks.
+        if (data_alive->find(addr) == data_alive->end())
             return false;
+        auto *copy = new std::map<uintptr_t, PerAddr>(*data_alive);
+        auto it = copy->find(addr);  // it's a different container, so do find again.
+        assert(it != copy->end());
         size_t size = it->second.size;
+        copy->erase(it);
+        // No one can modify data_alive between the following 2 lines, so we're safe.
+        auto *old = data_alive;
+        data_alive = copy;  // atomic!
+        delete old;
         heap.shrink(AddrSeg(addr, size));
-        data_alive.erase(it);
         return true;
     }
 
@@ -111,9 +122,8 @@ public:
     bool find_id_offset(uintptr_t addr, size_t &id, size_t &offset) {
         // Find the first starting address greater than `addr`, then it--
         // to get where `addr` falls in.
-        std::shared_lock<std::shared_timed_mutex> ul(mutex);
-        auto it = data_alive.upper_bound(addr);
-        if (it != data_alive.begin()) {
+        auto it = data_alive->upper_bound(addr);
+        if (it != data_alive->begin()) {
             it--;
             if (it->first + it->second.size > addr) {
                 id = it->second.id;
